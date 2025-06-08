3e70e6de09ff1e4fd3c7e797b2f52e37f0a98873
Bar Dahan <bardahan110@gmail.com>
2025-06-08 23:24:11 +0300

    changes

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,6 +1,9 @@
 export function BookPreview({ book }) {
   return (
     <article className="book-preview">
+      <div className="img-container">
+        {book.listPrice.isOnSale && (<img src="./assets/img/onsale.png" alt="On Sale" className="on-sale-icon"/>)}
+      </div>
       <h2>{book.title}</h2>
       {/* <img src={`../assets/BooksImages/${Math.floor(Math.random() * 20) + 1}.jpg`} alt="book-image"/> */}
       <img src={book.thumbnail} alt="book-image"/>
